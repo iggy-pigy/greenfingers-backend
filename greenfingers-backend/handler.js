2f810d23f9df64a8d5101364b17033155aead397
iggy-pigy <ilgakokorisa@gmail.com>
2020-06-26 13:58:04 +0100

    Added POST request

--- greenfingers-backend/handler.js
+++ greenfingers-backend/handler.js
@@ -12,4 +12,16 @@ app.get("/plants", function (req, res) {
   res.send({ plants: ["cactus", "rose"] })
 });
 
+app.post("/plants", function (req, res) {
+  const name = req.body.name;
+  const picture = req.body.picture;
+  const description = req.body.description;
+  const location = req.body.location;
+  const email = req.body.email;
+
+  res.json({ message: `Received a request to add plant ${name} with description ${description}, picture ${picture}, location ${location}, and e-mail ${email}` });
+});
+
+
+
 module.exports.plantApp = serverless(app);
