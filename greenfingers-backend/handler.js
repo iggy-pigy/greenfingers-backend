d0771481220fd44c766b26cac6df0195de9d7d77
iggy-pigy <ilgakokorisa@gmail.com>
2020-06-26 15:16:30 +0100

    Updated GET request with mysql connection query

--- greenfingers-backend/handler.js
+++ greenfingers-backend/handler.js
@@ -2,32 +2,52 @@ const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/plants", function (req, res) {
-  res.status(200).json({
-    plants: [{
-      "id": 1,
-      "name": "cactus",
-      "picture": "https://res.cloudinary.com/patch-gardens/image/upload/c_fill,f_auto,h_840,q_auto:good,w_840/v1515603160/products/cactus-45d5ec.jpg",
-      "description": "Desert plant that has thick, leafless stems covered in prickly spines or sharp spikes.",
-      "location": "Manchester",
-      "email": "cactus@example.com"
-    },
-    {
-      "id": 2,
-      "name": "fiddle-leaf fig",
-      "picture": "https://media.bunnings.com.au/Product-800x800/aae863bf-c09a-4642-8af4-5493a9ec1b16.jpg",
-      "description": "Has a tropical look with lyre-shaped leaves.",
-      "location": "Chorlton",
-      "email": "fig@example.com"
-    }]
-  })
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "plants"
 });
+
+app.get("/plants", function (req, res) {
+  console.log(req);
+  connection.query("SELECT * FROM Plant", function (err, data) {
+    if (err) {
+      res.status(500).json({
+        error: err
+      })
+    } else {
+      res.status(200).json({ plants: data })
+    }
+  })
+})
+
+/*res.status(200).json({
+  plants: [{
+    "id": 1,
+    "name": "cactus",
+    "picture": "https://res.cloudinary.com/patch-gardens/image/upload/c_fill,f_auto,h_840,q_auto:good,w_840/v1515603160/products/cactus-45d5ec.jpg",
+    "description": "Desert plant that has thick, leafless stems covered in prickly spines or sharp spikes.",
+    "location": "Manchester",
+    "email": "cactus@example.com"
+  },
+  {
+    "id": 2,
+    "name": "fiddle-leaf fig",
+    "picture": "https://media.bunnings.com.au/Product-800x800/aae863bf-c09a-4642-8af4-5493a9ec1b16.jpg",
+    "description": "Has a tropical look with lyre-shaped leaves.",
+    "location": "Chorlton",
+    "email": "fig@example.com"
+  }]
+})
+});*/
 
 app.post("/plants", function (req, res) {
   const name = req.body.name;
@@ -36,7 +56,7 @@ app.post("/plants", function (req, res) {
   const location = req.body.location;
   const email = req.body.email;
 
-  res.json({ message: `Received a request to add plant ${name} with description ${description}, picture ${picture}, location ${location}, and e-mail ${email}` });
+  res.status(200).json({ message: `Received a request to add plant ${name} with description ${description}, picture ${picture}, location ${location}, and e-mail ${email}` });
 });
 
 
