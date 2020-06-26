376bd4be66822b9e9b4a56da582949bec2b265c1
iggy-pigy <ilgakokorisa@gmail.com>
2020-06-26 13:49:11 +0100

    Added bodyParser

--- greenfingers-backend/handler.js
+++ greenfingers-backend/handler.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/plants", function (req, res) {
   res.send({ plants: ["cactus", "rose"] })
